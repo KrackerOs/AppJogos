759070a2195ea0422e68bb28e4a0b83cf334bcd1
KrackerOs <kleberisaac900@gmail.com>
2025-10-01 09:52:05 -0300

    koe

--- JOGOS/src/interface/Janela.java
+++ JOGOS/src/interface/Janela.java
@@ -1,6 +1,20 @@
 package interface;
+import javax.swing.JPanel;
 
-                                              
+
+import java.awt.CardLayout;
+
+
+
+import javax.swing.JFrame;
+                                
 public class Janela extends JFrame{
-    
+    public Janela(){
+        this.setBounds(0,0,1020,1080);
+        CardLayout controleTela = New 
+        CardLayout();
+        JPanel telas = NEW JPanel
+        (controleTela);
+        LoginPanel LoginPanel = New LoginPanel(telas, this);
+    }
 }
