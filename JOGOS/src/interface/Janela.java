28a85dfbe4e65ede205625fd67ebb90666c27cc6
KrackerOs <kleberisaac900@gmail.com>
2025-10-07 09:50:50 -0300

    uptade 5.0

--- JOGOS/src/interface/Janela.java
+++ JOGOS/src/interface/Janela.java
@@ -1,15 +1,11 @@
 package interface;
+
 import javax.swing.JPanel;
-
-
 import java.awt.CardLayout;
-
-
-
-import javax.swing.JFrame;
-                                
+import javax.swing.JFrame;                         
 public class Janela extends JFrame{
     public Janela(){
+
         this.setBounds(0,0,1020,1080);
         CardLayout controleTela = New 
         CardLayout();
@@ -17,5 +13,13 @@ public class Janela extends JFrame{
         (controleTela);
         LoginPanel LoginPanel = new LoginPanel(telas, this);
         PrincipalPanel PrincipalPanel = new Principalpanel(telas,this);
+
+        this.add(telas);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+        this.setVisible(true);
+        
+        
     }
 }
---
+++ JOGOS/src/interface/LoginPanel.java
@@ -0,0 +1,12 @@
+package interface;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class LoginPanel extends TelaPanel {
+      private JButton botaoLogin;
+      public LoginPanel(JPanel telas, JFrame Janela){
+        
+      }
+}
