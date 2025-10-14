07dc7354ec3c9b9da3265a3e06f44117dbbdf678
KrackerOs <kleberisaac900@gmail.com>
2025-10-14 09:51:36 -0300

    uptade

--- JOGOS/src/interface/TelaPanel.java
+++ JOGOS/src/interface/TelaPanel.java
@@ -1,3 +1,21 @@
-public class TelaPanel {
-    
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaPanel extends JPanel implements ActionListener{
+    private JPanel telas;
+    private CardLayout controeTela;
+    private JFrame janelas;
+
+    public TelaPanel(JPanel tela, JFrame janelas){
+ this.telas  = telas; 
+ this.controleTela = (CardLayout) telas.getLayout();
+ this.janelas = janelas;
+this.setBackground(Color.decode("#202028"));
+
+ Imagem botaoFechar = new Imagem("");
+    }
 }
